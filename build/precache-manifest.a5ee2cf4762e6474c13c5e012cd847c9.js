9a1ef31a485520dcc96b8f1bab0c903a0ad72d51
Abhishek Raj <54662059+iamraj45@users.noreply.github.com>
2022-08-18 21:27:14 +0530

    Add files via upload

---
+++ build/precache-manifest.a5ee2cf4762e6474c13c5e012cd847c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72e370af2a0105327c9b",
+    "url": "/static/css/main.8e8254a1.chunk.css"
+  },
+  {
+    "revision": "72e370af2a0105327c9b",
+    "url": "/static/js/main.72e370af.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "aa65d29876dd2e69b5d9",
+    "url": "/static/js/2.aa65d298.chunk.js"
+  },
+  {
+    "revision": "a850c70773011bd5f111",
+    "url": "/static/js/runtime~main.a850c707.js"
+  },
+  {
+    "revision": "4e4b5b21e3245d219c527f06ceb175e7",
+    "url": "/index.html"
+  }
+];
